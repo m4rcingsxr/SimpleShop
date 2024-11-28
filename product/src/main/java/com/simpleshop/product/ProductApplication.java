45541d95f7b298d49a4831e0ab37d339409cda06
Marcin Seweryn <marcinsewerynn@gmail.com>
2024-11-28 17:11:47 +0100

    integrated Spring Cloud Bus with RabbitMQ to enable event-driven configuration management across microservices

--- product/src/main/java/com/simpleshop/product/ProductApplication.java
+++ product/src/main/java/com/simpleshop/product/ProductApplication.java
@@ -1,10 +1,13 @@
 package com.simpleshop.product;
 
+import com.simpleshop.product.payload.ProductsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {ProductsContactInfoDto.class})
 @SpringBootApplication
 public class ProductApplication {
 
